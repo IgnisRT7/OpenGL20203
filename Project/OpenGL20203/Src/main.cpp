b72cb06f31be1cd2da94f920f45c36a76825b044
takuya yokoyama <ignisrt7mssp@gmail.com>
2022-03-23 18:04:11 +0900

    GLログ出力を作成

--- Project/OpenGL20203/Src/main.cpp
+++ Project/OpenGL20203/Src/main.cpp
@@ -3,7 +3,89 @@
 */
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <string>
+#include <iostream>
 #pragma comment(lib,"opengl32.lib")
+
+/**
+*	OpenGLからのメッセージを処理する
+*
+*	@param source	メッセージの発信者(OpenGL,Windows,シェーダーなど)
+*	@param type		メッセージの種類(エラー、警告など)
+*	@param id		メッセージを一位に識別する値
+*	@param severity	メッセージの重要度(高、中、低、最低)
+*	@param length	メッセージの文字数、負数ならメッセージは0終端
+*	@param message	メッセージの内容
+*   @param userparam コールバック設定時に指定したポインタ
+*/
+void GLAPIENTRY DebugCallback(GLenum source, GLenum type, GLuint id,
+	GLenum severity, GLsizei length, const GLchar* message, const void* userParam)
+{
+	const char* _source;
+	const char* _type; 
+	switch (source)
+	{
+	case GL_DEBUG_SOURCE_API:
+		_source = "API";
+		break;
+	case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
+		_source = "WINDOW SYSTEM";
+		break;
+	case GL_DEBUG_SOURCE_SHADER_COMPILER:
+		_source = "SHADER COMPILER";
+		break;
+	case GL_DEBUG_SOURCE_THIRD_PARTY:
+		_source = "THIRD PARTY";
+		break;
+	case GL_DEBUG_SOURCE_APPLICATION:
+		_source = "APPLICATION";
+		break;
+	case GL_DEBUG_SOURCE_OTHER:
+	default:
+		_source = "UNKNOWN";
+		break;
+	}
+
+	int typeColor = 0;
+	switch (type)
+	{
+	case GL_DEBUG_TYPE_PORTABILITY:
+	case GL_DEBUG_TYPE_PERFORMANCE:
+	case GL_DEBUG_TYPE_OTHER:
+	case GL_DEBUG_TYPE_MARKER:
+		typeColor = 30;
+		break;
+	case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+	case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+		typeColor = 33;
+		break;
+	case GL_DEBUG_TYPE_ERROR:
+		typeColor = 31;
+		break;
+	default:
+		typeColor = 30;
+		break;
+	}
+	std::string test("\033[31m aho \033[m");
+
+	std::string cmdStart;
+	cmdStart += "\033[" + std::to_string(typeColor) + "m ";
+
+	std::string cmdEnd;
+	cmdEnd += " \033[m";
+
+	if (length < 0)
+	{
+		std::cerr << "source=" + source << ": "
+			<< cmdStart << message << cmdEnd << "\n";
+	}
+	else
+	{
+		const std::string s(message, message +length);
+		std::cerr << "source=" + source << ": "
+			<< cmdStart << s << cmdEnd << "\n";
+	}
+}
 
 /**
 *	エントリーポイント
@@ -32,11 +114,14 @@ int main()
 	glfwMakeContextCurrent(window);
 
 	//OpenGL関数のアドレスを取得する
-	using pfnglClear = void(GLAPIENTRY*)(GLbitfield);
-	pfnglClear glClear = reinterpret_cast<pfnglClear>(glfwGetProcAddress("glClear"));
-	using pfnglClearColor = void(GLAPIENTRY*)(GLfloat, GLfloat, GLfloat, GLfloat);
-	pfnglClearColor glClearColor = reinterpret_cast<pfnglClearColor>(glfwGetProcAddress("glClearColor"));
+	if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
+	{
+		glfwTerminate();
+		return 1;
+	}
 
+	glDebugMessageCallback(DebugCallback, nullptr);
+	glDrawElements(GL_LINES, 0, 0, nullptr);
 	//メインループ
 	while (!glfwWindowShouldClose(window))
 	{
