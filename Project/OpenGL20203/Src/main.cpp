6ae79a856b9c879c62f28fe05a5f8423daa0783a
IgnisRT7 <ignisrt7mssp@gmail.com>
2022-04-02 21:40:43 +0900

    ライティング用シェーダの追加

--- Project/OpenGL20203/Src/main.cpp
+++ Project/OpenGL20203/Src/main.cpp
@@ -257,7 +257,7 @@ int main()
 		//
 
 		glEnable(GL_DEPTH_TEST);
-		glEnable(GL_CULL_FACE);
+		glDisable(GL_CULL_FACE);
 		glClearColor(0.1f, 0.3f, 0.5f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
